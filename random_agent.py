6a061281d7cb4839fd644e4ffe6127bac94ee924
Ethan Brooks <ethanabrooks@gmail.com>
2017-04-21 18:05:41 -0400

    added random agent and launch file

---
+++ random_agent.py
@@ -0,0 +1,12 @@
+import rospy
+import numpy as np
+from gazebo import Gazebo
+
+env = Gazebo(observation_range=(-1, 1), action_range=(-1, 1), action_shape=(3,))
+
+env.reset()
+while not rospy.is_shutdown():
+    _, _, done, _ = env.step(*list(np.random.rand(3)))
+    if done:
+        env.reset()
+
