e13d03a2f212cb47d53be8e516acc04e9b3fc20e
Ethan Brooks <ethanabrooks@gmail.com>
2017-05-01 12:10:19 -0400

    learns with cartpole and rmsprop but a little slow

--- a3c.py
+++ a3c.py
@@ -252,7 +252,8 @@ should be computed.
             grads_and_vars = list(zip(grads, self.network.var_list))
             inc_step = self.global_step.assign_add(tf.shape(pi.x)[0])
 
-            opt = tf.train.RMSPropOptimizer(1e-4) # / tf.floor(tf.to_float(self.global_step) * 1e-6 + 1))
+            learning_rate = 1e-4
+            opt = tf.train.RMSPropOptimizer(learning_rate)  # / tf.floor(tf.to_float(self.global_step) * 1e-6 + 1))
             self.train_op = tf.group(opt.apply_gradients(grads_and_vars), inc_step)
             self.summary_writer = None
             self.local_steps = 0
