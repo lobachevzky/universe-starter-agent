1f58588d4632c18744825d9a166f3b255eefede4
Ethan Brooks <ethanabrooks@gmail.com>
2017-05-03 13:46:59 -0400

    changed ardrone_sim to  cvg_sim_gazebo

--- random_agent.py
+++ random_agent.py
@@ -7,8 +7,9 @@ env = Gazebo(observation_range=(-1, 1), action_range=(-1, 1), action_shape=(3,))
 
 env.reset()
 while not rospy.is_shutdown():
-    action = np.random.rand(3)
-    _, reward, done, _ = env.step(action)
-    if done:
-        env.reset()
+    pass
+    # action = np.random.rand(3)
+    # _, reward, done, _ = env.step(action)
+    # if done:
+        # env.reset()
 
