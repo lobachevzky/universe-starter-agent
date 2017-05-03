4fa7e5f07ec5ad333a870893ce9618f42c33b9d7
Ethan Brooks <ethanabrooks@gmail.com>
2017-05-03 14:46:06 -0400

    added new simple_maze and broke directory

--- random_agent.py
+++ random_agent.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
+import rospy
 from gazebo import Gazebo
+from std_srvs import srv
 
 env = Gazebo(observation_range=(-1, 1), action_range=(-1, 1), action_shape=(3,))
-
 env.reset()
+
+# rospy.loginfo('resetting...')
+# rospy.wait_for_service('gazebo/reset_world')
+# rospy.ServiceProxy('gazebo/reset_world', srv.Empty)()
+# rospy.loginfo('reset')
 while not rospy.is_shutdown():
-    pass
-    # action = np.random.rand(3)
-    # _, reward, done, _ = env.step(action)
-    # if done:
-        # env.reset()
+    # pass
+    action = np.random.rand(3)
+    _, reward, done, _ = env.step(action)
+    if done:
+        env.reset()
 
